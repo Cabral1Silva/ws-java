eaac689cc7639aeaed7d5237e5786473f1ba5575
cabral1silva <rafaelcabralsilva1@gmail.com>
2023-07-26 21:07:38 -0300

    tipo enum sabores pizza especial

---
+++ 02 - Gerador de conta Pizzaria/src/entidade/enums/SaboresEspecial.java
@@ -0,0 +1,20 @@
+package entidade.enums;
+
+public enum SaboresEspecial {
+	
+	
+	
+	
+	CAMARÃO,	
+	CAVIAR,
+	CARNE_COM_BANANA_DA_TERRA,
+	PIZZA_DE_ROSAS,
+	CHOCOLATE_AMARGO_E_MARACUJA,
+
+	
+	
+	
+	
+	
+
+}
