b9c187701e2a3a0078da0d62bb751b8f16b917fa
cabral1silva <rafaelcabralsilva1@gmail.com>
2023-07-26 22:06:58 -0300

    update classe pizza

--- 02 - Gerador de conta Pizzaria/src/entidade/Pizza.java
+++ 02 - Gerador de conta Pizzaria/src/entidade/Pizza.java
@@ -1,12 +1,16 @@
 package entidade;
 
+import entidade.enums.SaboresComum;
+
 public class Pizza {
 	
 	private Double  preco;
 	private Boolean vegano; // verificar se é vegano ou não
+	private SaboresComum sabores;
 	
 	
-	public Pizza(Boolean vegano) {
+	
+	public Pizza(Boolean vegano,SaboresComum sabores ) {
 		
 		//  quando for instanciado o construtor precisa sabe se é vegano ou não 
 		// o preco não é necessario pois ja esta definido apesar de mudar se for vegano ou não!
@@ -16,12 +20,35 @@ public class Pizza {
 			this.preco = 100.00;
 		}else {
 			this.preco = 50.00;
-		}		
+		}	
+		
+		
+		this.sabores = sabores;
+		
+		
 	}
 	
+	
+	
+	public SaboresComum getSabores() {
+		return sabores;
+	}
+
+
+
+	
+
 	public void massaPizza(){			
 		
 		System.out.println(this.preco);
+		
+	}
+	
+	
+	public String toString() {
+		
+		String tipoMassa = vegano? "Massa Vegana" : "Massa Tradicional";
+		return "Pizza: " + tipoMassa + ", Sabor: " + sabores;
 		
 	}
 
